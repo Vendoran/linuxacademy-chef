1f2ff14f2962d3a378ccaf467d1dc661654ebc69
Aaron <aaron.lowe@ge.com>
2016-09-22 21:28:35 +0000

    Updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["aaron-lowe-ge-com2"] = { "site_title" => "Aaron2s we
 default["apache"]["sites"]["aaron-lowe-ge-com2b"] = { "site_title" => "Aaron2bs website comming soon!", "port" => 80, "domain" => "aaron-lowe-ge-com2b.mylabserver.com" }
 default["apache"]["sites"]["aaron-lowe-ge-com3"] = { "site_title" => "Aaron3s website", "port" => 80, "domain" => "aaron-lowe-ge-com3.mylabserver.com" }
 
+default["author"]["name"] = "aaron"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
